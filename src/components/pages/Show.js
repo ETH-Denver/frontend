74307fe2c7c27854aa30338cc0d3eeba82124d76
Tyler Tedesco <s_tylert@icloud.com>
2024-02-25 16:10:00 -0700

    Refactor imports

--- src/components/pages/Show.js
+++ src/components/pages/Show.js
@@ -1,8 +1,12 @@
 import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import { Box, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Stack,
+} from "@mui/material";
 
 const CustomTextField = (props) => (
   <TextField
