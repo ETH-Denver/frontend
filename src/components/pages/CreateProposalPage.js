d07d38e1d561d77038659624f26186583d632404
Tyler Tedesco <s_tylert@icloud.com>
2024-02-26 18:12:21 -0700

    Create initial form

--- src/components/pages/CreateProposalPage.js
+++ src/components/pages/CreateProposalPage.js
@@ -1,14 +1,49 @@
 import { Container, TextField } from "@mui/material";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ProposalForm = () => {
+  const [executionDate, setExecutionDate] = useState(new Date());
+  const [location, setLocation] = useState("");
+  const [content, setContent] = useState("");
+
   return (
-    <form>
-      <TextField></TextField>
-      <TextField></TextField>
-      <TextField></TextField>
-      <TextField></TextField>
-      <TextField></TextField>
-    </form>
+    <Container>
+      <form>
+        <TextField
+          inputProps={{ maxLength: 12 }}
+          label="Location"
+          onChange={(e) => setLocation(e.target.value)}
+          required
+          variant="outlined"
+          color="secondary"
+          type="password"
+          value={location}
+          fullWidth
+          sx={{ mb: 3 }}
+        />
+        <TextField
+          inputProps={{ maxLength: 12 }}
+          label="Content"
+          onChange={(e) => setLocation(e.target.value)}
+          required
+          variant="outlined"
+          color="secondary"
+          type="content"
+          value={content}
+          fullWidth
+          sx={{ mb: 3 }}
+        />
+        <DatePicker
+          showIcon
+          selected={executionDate}
+          onChange={(date) => setExecutionDate(date)}
+          icon="fa fa-calendar"
+          label="Execution Date"
+        />
+      </form>
+    </Container>
   );
 };
 
