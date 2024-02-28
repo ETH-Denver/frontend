da7331ffaf54762293d4ec6c85899897da101e88
Tyler Tedesco <s_tylert@icloud.com>
2024-02-27 19:43:30 -0700

    Add places auto complete

--- src/components/baseComponents/AutoComplete.js
+++ src/components/baseComponents/AutoComplete.js
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
+import Autocomplete from "react-google-autocomplete";
 
-import {
-  GeoapifyGeocoderAutocomplete,
-  GeoapifyContext,
-} from "@geoapify/react-geocoder-autocomplete";
-import "@geoapify/geocoder-autocomplete/styles/minimal.css";
-
-export const AutoComplete = () => {
-  const apiKey = "2b35b28f8163427c9fc6e124f3abd05f";
-  function sendGeocoderRequest(value, geocoder) {
-    console.log(value, value); //the search term
-    return geocoder.sendGeocoderRequest(value);
-  }
-
-  function sendPlaceDetailsRequest(feature, geocoder) {
-    console.log(feature); // the result of the search
-    return geocoder.sendPlaceDetailsRequest(feature);
-  }
-  return (
-    <GeoapifyContext apiKey={apiKey}>
-      <GeoapifyGeocoderAutocomplete
-        placeholder="Enter address here"
-        sendGeocoderRequestFunc={sendGeocoderRequest}
-        sendPlaceDetailsRequestFunc={sendPlaceDetailsRequest}
-      />
-    </GeoapifyContext>
-  );
-};
+<Autocomplete
+  apiKey="AIzaSyBkk4pDx-Vz48tU4tJ1iMKMPTcs2_csJt0"
+  style={{ width: "90%" }}
+  onPlaceSelected={(place) => {
+    console.log(place);
+  }}
+  options={{
+    types: ["(regions)"],
+    componentRestrictions: { country: "ru" },
+  }}
+  defaultValue="United States"
+/>;
--- src/components/pages/CreateProposalPage.js
+++ src/components/pages/CreateProposalPage.js
@@ -1,23 +1,43 @@
-import { Container, TextField } from "@mui/material";
-import { useState } from "react";
+import { Button, Container, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { AutoComplete } from "../baseComponents/AutoComplete";
+import Autocomplete from "react-google-autocomplete";
 
 const ProposalForm = () => {
   const [executionDate, setExecutionDate] = useState(new Date());
   const [location, setLocation] = useState("");
   const [content, setContent] = useState("");
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(executionDate, location, content, lat, long);
+  }
+  useEffect(() => {
+    console.log(lat, long);
+  }, [lat, long]);
 
   return (
     <Container>
-      <AutoComplete />
-      <form>
+      <Autocomplete
+        style={{ width: "90%" }}
+        apiKey="AIzaSyBkk4pDx-Vz48tU4tJ1iMKMPTcs2_csJt0"
+        onPlaceSelected={(place) => {
+          console.log(place.geometry.location.lat());
+          console.log(place.geometry.location.lng());
+        }}
+        componentRestrictions={{ country: "us" }}
+        options={{
+          types: ["geocode", "establishment"],
+        }}
+      />
+      <form onSubmit={handleSubmit}>
         <TextField
           inputProps={{ maxLength: 12 }}
           label="Location"
           onChange={(e) => setLocation(e.target.value)}
-          required
           variant="outlined"
           color="secondary"
           type="password"
@@ -28,8 +48,7 @@ const ProposalForm = () => {
         <TextField
           inputProps={{ maxLength: 12 }}
           label="Content"
-          onChange={(e) => setLocation(e.target.value)}
-          required
+          onChange={(e) => setContent(e.target.value)}
           variant="outlined"
           color="secondary"
           type="content"
@@ -44,6 +63,9 @@ const ProposalForm = () => {
           icon="fa fa-calendar"
           label="Execution Date"
         />
+        <Button variant="outlined" color="secondary" type="submit">
+          Register
+        </Button>
       </form>
     </Container>
   );
